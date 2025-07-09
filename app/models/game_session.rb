8ae66f30512ea4603b58f6177fd923d3c2b520f3
Ankush Kumar <ankushkumar@Ankushs-MacBook-Pro.local>
2025-07-09 16:31:47 +0530

    validation fix

--- app/models/game_session.rb
+++ app/models/game_session.rb
@@ -1,3 +1,4 @@
 class GameSession < ApplicationRecord
+  validates :score, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 1_000_000 }
   belongs_to :user
 end
